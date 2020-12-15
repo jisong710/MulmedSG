b7691d003b4e6fa975d3aabbcf6920c59c81192b
aji sasongko <57887588+jisong710@users.noreply.github.com>
2020-12-15 21:00:24 +0700

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,6 +5,10 @@ def volumeLimEmpat(panjang,lebar,tinggi):
     return panjang*lebar*tinggi/3
 def volumetab(jarijari,tinggi):
     return 3.14*jarijari*jarijari*tinggi
+def volumeBola(jarijari):
+    return 3.14*(jarijari**3)*4/3
+def volumekrucut(jarijari,tinggi):
+    return 3.14*(jarijari**2)*tinggi/3
 def menu():
     jawaban = 999999
     while(jawaban != 0):
@@ -12,6 +16,8 @@ def menu():
         print("1. volume prisma segi tiga")
         print("2. volume limas segi empat")
         print("3. volume tabung")
+        print("4. volume Bola")
+        print("5. volume Kerucut")
         print("0. keluar")
         jawaban = int(input("jawaban anda : "))
         if jawaban == 1 :
@@ -30,6 +36,15 @@ def menu():
             jarijari = float(input("jari jari :"))
             tinggi = float(input("tinggi :"))
             print("volumenya adalah : " + str(volumetab(jarijari,tinggi)))
+            input("Press Enter to continue...")
+        elif jawaban == 4:
+            jarijari = float(input("jari jari :")
+            print("volumenya adalah : " + str(volumeBola(jarijari)))
+            input("Press Enter to continue...")
+        elif jawaban == 5:
+            jarijari = float(input("jari jari :"))
+            tinggi = float(input("tinggi :"))
+            print("volumenya adalah : " + str(volumekrucut(jarijari,tinggi)))
             input("Press Enter to continue...")
         elif jawaban == 0:
             print("terima kasih")
